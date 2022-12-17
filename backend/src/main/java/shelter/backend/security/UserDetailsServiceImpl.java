5166522de3070b33ef7665686317733576976c29
mczerwiak06 <michal.czerwiak@gmail.com>
2022-12-17 13:35:48 +0100

    Bug fix - tworzenie beanu z UserDetailsServiceImpl

--- backend/src/main/java/shelter/backend/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/shelter/backend/security/UserDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import shelter.backend.models.Role;
 import shelter.backend.models.User;
 import shelter.backend.repositories.UserRepository;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
 
+@Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
