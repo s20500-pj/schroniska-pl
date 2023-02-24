cf4d23855bdd67e40619777e3e5e4dc1aeb14da1
mczerwiak06 <michal.czerwiak@gmail.com>
2023-02-24 08:18:44 +0100

    Front: Stona startowa + routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./layout/Navbar";
+import Home from "./layout/Home";
+import AddUser from "./users/AddUser";
+import AddShelter from "./users/AddShelter";
+import Login from "./users/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/adduser" element={<AddUser />} />
+            <Route exact path="/addshelter" element={<AddShelter />} />
+            <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ frontend/src/layout/Navbar.js
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        schroniska-pl
+                    </a>
+                    <Link className="btn btn-outline-danger" to="/">Home</Link>
+                    <Link className="btn btn-outline-light" to="/adduser">Zarejestruj się</Link>
+                    <Link className="btn btn-outline-light" to="/addshelter">Zarejestruj schronisko</Link>
+                    <Link className="btn btn-outline-light" to="/login">Zaloguj się</Link>
+                </div>
+            </nav>
+        </div>
+    )
+}
