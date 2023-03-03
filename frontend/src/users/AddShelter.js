565e732f0d08312e298c305872763524e6f925bd
s13146-pj <56793702+s13146@users.noreply.github.com>
2023-03-03 11:17:50 +0100

    dodalam style w addShelter

--- frontend/src/users/AddShelter.js
+++ frontend/src/users/AddShelter.js
@@ -41,18 +41,19 @@ export default function AddShelter() {
     const {email, password, shelterName, street, city, postalCode, buildingNumber, flatNumber, phone, krsNumber} = shelter
 
     return (
-        <div className="container">
-            <div className="row">
+        <div className="bg-background-pattern bg-opacity-20 max-w-none">
+            <div className="px-10 font-display bg-white bg-opacity-80">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <h2 className="text-center m-4">Zarejestruj schronisko</h2>
-                    <form onSubmit={(e)=>onSubmit(e)}>
-                        <div className="mb-3">
-                            <label htmlFor="Shelter name" className="form-label">
+                    <h2 className="text-center text-2xl text-orange font-bold p-10">Zarejestruj schronisko</h2>
+                    <form onSubmit={(e)=>onSubmit(e)} className="w-full max-w-lg m-auto py-10">
+                        <div className="flex flex-wrap">
+                            <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label htmlFor="Shelter name" className="block uppercase tracking-wide text-brown text-xs font-bold mb-2">
                                 Nazwa schroniska:
                             </label>
                             <input
                                 type={"text"}
-                                className="form-control"
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                 placeholder="Podaj nazwę schroniska."
                                 name="shelterName"
                                 value={shelterName}
@@ -60,12 +61,12 @@ export default function AddShelter() {
                                 required
                             />
                             <div className="mb-3">
-                                <label htmlFor="Email" className="form-label">
+                                <label htmlFor="Email" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     E-mail:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj adres e-mail."
                                     name="email"
                                     value={email}
@@ -74,12 +75,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Password" className="form-label">
+                                <label htmlFor="Password" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Hasło:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Wpisz hasło."
                                     name="password"
                                     value={password}
@@ -88,12 +89,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Street" className="form-label">
+                                <label htmlFor="Street" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Ulica:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj ulicę."
                                     name="street"
                                     value={street}
@@ -102,12 +103,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="City" className="form-label">
+                                <label htmlFor="City" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Miasto:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj miasto."
                                     name="city"
                                     value={city}
@@ -116,12 +117,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Postal code" className="form-label">
+                                <label htmlFor="Postal code" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Kod pocztowy:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj kod pocztowy."
                                     name="postalCode"
                                     value={postalCode}
@@ -130,12 +131,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Building number" className="form-label">
+                                <label htmlFor="Building number" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Numer budynku:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj numer budynku."
                                     name="buildingNumber"
                                     value={buildingNumber}
@@ -144,12 +145,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Flat number" className="form-label">
+                                <label htmlFor="Flat number" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Numer mieszkania:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj numer mieszkania."
                                     name="flatNumber"
                                     value={flatNumber}
@@ -157,12 +158,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="Phone" className="form-label">
+                                <label htmlFor="Phone" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Telefon kontaktowy:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj numer telefonu kontaktowego."
                                     name="phone"
                                     value={phone}
@@ -171,12 +172,12 @@ export default function AddShelter() {
                                 />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="KRS_number" className="form-label">
+                                <label htmlFor="KRS_number" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                                     Numer KRS:
                                 </label>
                                 <input
                                     type={"text"}
-                                    className="form-control"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                     placeholder="Podaj numer KRS."
                                     name="krsNumber"
                                     value={krsNumber}
@@ -185,11 +186,12 @@ export default function AddShelter() {
                                 />
                             </div>
                         </div>
-                        <button type="submit" className="btn btn-outline-primary">
-                            Zarejestruj
+                        </div>
+                        <button type="submit"  className="px-10 py-2 m-5 border-2 border-orange rounded-2xl bg-white  hover:bg-orange text-white active:bg-brown ">
+                            <p className="py-15 justify-center text-base	 text-center text-brown font-medium	">Zarejestruj</p>
                         </button>
-                        <Link type="submit" className="btn btn-danger mx-2" to="/">
-                            Anuluj
+                        <Link type="submit" className="px-10 py-2 m-5 border-2 border-orange rounded-2xl bg-white hover:bg-orange text-white active:bg-brown " to="/">
+                            <p className="py-15 justify-center text-base	 text-center text-brown font-medium	">Anuluj</p>
                         </Link>
                     </form>
                 </div>
--- frontend/src/users/AddUser.js
+++ frontend/src/users/AddUser.js
@@ -193,7 +193,6 @@ export default function AddUser() {
                         </button>
                         <Link type="submit" className="px-10 py-2 m-5 border-2 border-orange rounded-2xl bg-white hover:bg-orange text-white active:bg-brown " to="/">
                             <p className="py-15 justify-center text-base	 text-center text-brown font-medium	">Anuluj</p>
-
                         </Link>
                     </form>
         </div>
