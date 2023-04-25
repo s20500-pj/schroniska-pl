bb120f2b955dbe8447029698c02168d62a499951
s13146-pj <56793702+s13146@users.noreply.github.com>
2023-04-25 10:52:35 +0200

    wyswietlanie danyc  schroniska

--- frontend/src/lists/ShelterlList.js
+++ frontend/src/lists/ShelterlList.js
@@ -32,7 +32,11 @@ const columns = [
                 accessor: "id",
                 Cell: ({value}) => (
                     <Link to={`/shelterDetails/${value}`}>
-                        Zobacz szczegóły/ do implementacji
+                        <button type="submit"
+                                className="px-10 py-2 m-5 border-2 border-orange rounded-2xl bg-white  hover:bg-orange text-white active:bg-brown ">
+                            <p className="py-15 justify-center text-base	 text-center text-brown font-medium	">Dane schroniska
+                                </p>
+                        </button>
                     </Link>
                 ),
             }
