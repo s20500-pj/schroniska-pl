68a84a5c73002b0aa791835c51eced5078c73d85
s13146-pj <56793702+s13146@users.noreply.github.com>
2023-04-30 23:33:55 +0200

    dodanie inforamcji po rejstracji

---
+++ frontend/src/users/unlogged/Modal.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
+function Modal({setOpenModal}, navigate) {
+
+    return (<>
+            <div
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50
+                outline-none focus:outline-none">
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    <div
+                        className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        <div
+                            className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-2xl font-semibold">
+                                Rejestracja </h3>
+                            <button
+                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                onClick={() => setOpenModal(false)}
+                            >
+                    <span
+                        className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                            </button>
+                        </div>
+                        <div className="relative p-6 flex-auto">
+                            <p className="my-4 text-orange text-lg leading-relaxed">
+                                Rejestracja udana, dziękujemy.
+                            </p>
+                        </div>
+                        <div
+                            className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                                className="bg-orange text-white active:bg-emerald-600 font-display uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setOpenModal(false)}
+                            >
+                                Zamknij
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+    );
+}
+
+export default Modal;
