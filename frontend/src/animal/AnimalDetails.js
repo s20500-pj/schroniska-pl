5afd63b992b1e8df5e29ac155238edf98ac2981c
s20500-pj <s20500@pjwstk.edu.pl>
2023-05-07 22:02:45 +0200

    refractor

--- frontend/src/animal/AnimalDetails.js
+++ frontend/src/animal/AnimalDetails.js
@@ -344,7 +344,7 @@ export default function AnimalDetails() {
                                     <input
                                         type={"text"}
                                         className="appearance-none block w-full bg-gray-200 text-brown border border-orange rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                        name="firstName"
+                                        name="name"
                                         defaultValue={animal.name}
                                         onChange={handleInput}
                                     />
