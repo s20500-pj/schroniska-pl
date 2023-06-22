c7d2ca7ba8746da8d7d02260736e046abe27cc39
s13146-pj <56793702+s13146@users.noreply.github.com>
2023-06-22 17:13:36 +0200

    zwiekszenie przestrzeni

--- frontend/src/users/Login/Login.js
+++ frontend/src/users/Login/Login.js
@@ -53,10 +53,10 @@ function Login({loggingInfo}) {
     return (
         <div className="bg-background-pattern bg-opacity-20 max-w-none">
             {modalOpen && <PopupErrorLogin setOpenModal={setModalOpen} error={error}/>}
-            <div className="px-10 font-display bg-white bg-opacity-90">
+            <div className="px-10 font-display bg-white bg-opacity-90 h-[34rem]">
                 <h2 className="text-center text-2xl text-orange font-bold p-10">Zaloguj</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className="block w-80 m-auto">
+                    <div className="block w-80 m-auto pt-8">
                         <div className="px-3">
                             <label htmlFor="email"
                                    className="block uppercase tracking-wide text-brown text-xs font-bold mb-2">Email:</label>
@@ -73,7 +73,7 @@ function Login({loggingInfo}) {
                                    id="password" value={password} onChange={handlePasswordChange}/>
                         </div>
                         <button type="submit"
-                                className=" px-10 py-2 m-5 border-2 border-orange rounded-2xl bg-white  hover:bg-orange text-white active:bg-brown mb-10"
+                                className=" px-10 py-2 mx-5 mt-10 border-2 border-orange rounded-2xl bg-white  hover:bg-orange text-white active:bg-brown mb-10"
                                 onClick={() => {
                                     loggingInfo(storageFilled);
                                 }}
